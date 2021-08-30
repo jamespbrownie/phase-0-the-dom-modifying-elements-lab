00f30032b5f16777b33ac99451d401337b73463c
James Brownie <jamespbrownie@gmail.com>
2021-08-30 10:30:50 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+main.remove();
+//first test complete 
+
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+
+newHeader.id = "victory"
+victory.innerHTML = "<h1>YOUR-NAME is the champion</h1>"
